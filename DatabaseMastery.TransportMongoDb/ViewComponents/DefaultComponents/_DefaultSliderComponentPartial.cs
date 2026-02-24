ddbe5799e386b3e9b31afb23b3b6f3b0fb2709f1
BULUTS <BULUTS@BULUTS>
2026-02-24 15:46:43 +0300

    Add project files.

---
+++ DatabaseMastery.TransportMongoDb/ViewComponents/DefaultComponents/_DefaultSliderComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DatabaseMastery.TransportMongoDb.ViewComponents.DefaultComponents
+{
+    public class _DefaultSliderComponentPartial: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
