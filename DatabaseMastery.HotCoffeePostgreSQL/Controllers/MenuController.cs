6747fbdf63fd9477d9a4905c48aa106630f8c002
BULUTS <BULUTS@BULUTS>
2026-03-11 14:04:32 +0300

    59.menu ve footer component partial

---
+++ DatabaseMastery.HotCoffeePostgreSQL/Controllers/MenuController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DatabaseMastery.HotCoffeePostgreSQL.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DatabaseMastery.HotCoffeePostgreSQL/ViewComponents/MenuViewComponents/_MenuListComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseMastery.DinnerMenuPostgreSQL.ViewComponents.MenuViewComponents
+{
+    public class _MenuListComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
